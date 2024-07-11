280305db0b15f1b36155b2f1cf5aed850a739a88
Your Name <you@example.com>
2024-07-11 20:59:03 +0600

    ad

--- src/hooks/consts.ts
+++ src/hooks/consts.ts
@@ -1,5 +1,3 @@
-// export const PUBLIC_API_URL = 'https://admin.unifive.pro/unifive';
-export const PUBLIC_API_URL = 'http://185.129.51.52/unifive/';
-// export const PUBLIC_BASE_URL = 'https://admin.unifive.pro';
-export const PUBLIC_BASE_URL = 'http://185.129.51.52';
+export const PUBLIC_API_URL = 'https://admin.unifive.pro/unifive';
+export const PUBLIC_BASE_URL = 'https://admin.unifive.pro';
 export const I18N_LANG_KEY = 'i18nextLng'
