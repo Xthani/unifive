d35b6685eb3d162730cacc285cdcc36656007198
Your Name <you@example.com>
2024-07-11 20:49:07 +0600

    fix

--- src/hooks/consts.ts
+++ src/hooks/consts.ts
@@ -1,3 +1,5 @@
-export const PUBLIC_API_URL = 'https://admin.unifive.pro/unifive';
-export const PUBLIC_BASE_URL = 'https://admin.unifive.pro';
+// export const PUBLIC_API_URL = 'https://admin.unifive.pro/unifive';
+export const PUBLIC_API_URL = 'http://185.129.51.52/unifive';
+// export const PUBLIC_BASE_URL = 'https://admin.unifive.pro';
+export const PUBLIC_BASE_URL = 'http://185.129.51.52';
 export const I18N_LANG_KEY = 'i18nextLng'
